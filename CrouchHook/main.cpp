19858b408a98bb7d2380cdf0ef9683275d5b2090
Srd Vladimir <vladimir.srd@gmail.com>
2021-06-25 12:38:20 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

--- CrouchHook/main.cpp
+++ CrouchHook/main.cpp
@@ -5,7 +5,7 @@ typedef int(__fastcall* CrouchFunction)(int* param_1, void* EDX, int param_1_00)
 
 CrouchFunction CrouchTrampoline;
 
-int __fastcall CrouchHooked(int* param_1, void* EDX, int param_1_00)
+int __fastcall CrouchHookedd(int* param_1, void* EDX, int param_1_00)
 {
 	if (*reinterpret_cast<DWORD*>(0xB6F5F0) == param_1_00) 
 		return 1;
@@ -20,7 +20,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 	{
 		if (MH_Initialize() == MH_OK)
 		{
-			MH_CreateHook(reinterpret_cast<CrouchFunction>(0x694390), &CrouchHooked, reinterpret_cast<LPVOID*>(&CrouchTrampoline));
+			MH_CreateHook(reinterpret_cast<CrouchFunction>(0x694390), &CrouchHookedd, reinterpret_cast<LPVOID*>(&CrouchTrampoline));
 			MH_EnableHook(MH_ALL_HOOKS);
 		}
 	}
