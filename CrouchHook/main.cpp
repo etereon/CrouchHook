02e4066aeb184c04d23314f64022b1aea478327e
Srd Vladimir <vladimir.srd@gmail.com>
2021-06-25 22:31:45 +0300

    update

--- CrouchHook/main.cpp
+++ CrouchHook/main.cpp
@@ -1,32 +1,37 @@
 #include <Windows.h>
 #include "MinHook.h"
 
-typedef int(__fastcall* CrouchFunction)(int* param_1, void* EDX, int param_1_00);
+typedef bool(__thiscall* CrouchFunction)(void*, void*);
 
+WNDPROC WndProcTrampoline;
 CrouchFunction CrouchTrampoline;
 
-int __fastcall CrouchHookedd(int* param_1, void* EDX, int param_1_00)
+bool state;
+
+LRESULT CALLBACK WndProcHooked(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	if (*reinterpret_cast<DWORD*>(0xB6F5F0) == param_1_00) 
-		return 1;
-	else
-		return CrouchTrampoline(param_1, EDX, param_1_00);
+	if (uMsg == WM_KEYUP) {
+		if (wParam == VK_F12 && *reinterpret_cast<byte*>(*reinterpret_cast<DWORD*>(0xB6F5F0) + 0x46F) == 128) {
+			state = !state;
+		}
+	}
+	return WndProcTrampoline(hWnd, uMsg, wParam, lParam);
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
-{
-	DisableThreadLibraryCalls(hinstDLL);
-	if (fdwReason == DLL_PROCESS_ATTACH)
-	{
-		if (MH_Initialize() == MH_OK)
-		{
-			MH_CreateHook(reinterpret_cast<CrouchFunction>(0x694390), &CrouchHookedd, reinterpret_cast<LPVOID*>(&CrouchTrampoline));
+bool __fastcall CrouchHooked(void* ecx, void* edx, void* ped) {
+	if (state && *reinterpret_cast<void**>(0xB6F5F0) == ped) return 1;
+	return CrouchTrampoline(ecx, ped);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+	if (fdwReason == DLL_PROCESS_ATTACH) {
+		DisableThreadLibraryCalls(hinstDLL);
+		if (MH_Initialize() == MH_OK) {
+			MH_CreateHook(reinterpret_cast<WNDPROC>(0x747EB0), &WndProcHooked, reinterpret_cast<LPVOID*>(&WndProcTrampoline));
+			MH_CreateHook(reinterpret_cast<CrouchFunction>(0x694390), &CrouchHooked, reinterpret_cast<LPVOID*>(&CrouchTrampoline));
 			MH_EnableHook(MH_ALL_HOOKS);
 		}
 	}
-	else
-	{
-		MH_Uninitialize();
-	}
+	else MH_Uninitialize();
 	return TRUE;
 }
